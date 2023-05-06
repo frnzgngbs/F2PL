9c8b32d3c1f40b8b46b9c10da923835d5a1c061f
Franz Genegobis <genegobis.franz@gmail.com>
2023-05-06 18:34:48 +0800

    FOURTH COMMIT - MADE THE REGISTRATION ACTIVITY

--- app/src/main/java/com/example/f2pl/Login_Activity.java
+++ app/src/main/java/com/example/f2pl/Login_Activity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity implements View.OnClickListener{
 
     Button login, signup;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,11 @@ public class Login_Activity extends AppCompatActivity implements View.OnClickLis
 
         login =  findViewById(R.id.Login);
         signup = findViewById(R.id.Signup);
+        back = findViewById(R.id.back);
 
         login.setOnClickListener(this);
         signup.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +35,10 @@ public class Login_Activity extends AppCompatActivity implements View.OnClickLis
         else if(view.getId() == R.id.Signup) {
             Toast.makeText(this, "Sign Up has been pressed", Toast.LENGTH_SHORT).show();
             a = new Intent(Login_Activity.this, Register_Activity.class);
+            startActivity(a);
+        }else if(view.getId() == R.id.back) {
+            Toast.makeText(this, "Back button has been pressed", Toast.LENGTH_SHORT).show();
+            a = new Intent(Login_Activity.this, Login_Choice.class);
             startActivity(a);
         }
     }
--- app/src/main/java/com/example/f2pl/Register_Activity.java
+++ app/src/main/java/com/example/f2pl/Register_Activity.java
@@ -2,13 +2,53 @@ package com.example.f2pl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register_Activity extends AppCompatActivity {
+public class Register_Activity extends AppCompatActivity implements View.OnClickListener {
 
+    ImageButton back;
+    Button create_acc;
+    EditText name,username,password,birth, mpin;
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        back = findViewById(R.id.btnback);
+        create_acc = findViewById(R.id.btncreatacc);
+        name = findViewById(R.id.registername);
+        username = findViewById(R.id.registerusername);
+        password = findViewById(R.id.registerpassword);
+        birth = findViewById(R.id.registerbirth);
+        mpin = findViewById(R.id.registermpin);
+        login = findViewById(R.id.txtlogin);
+
+        back.setOnClickListener(this);
+        create_acc.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent a;
+        if(view.getId() == back.getId())  {
+            Toast.makeText(this, "Back button has been pressed", Toast.LENGTH_SHORT).show();
+            a = new Intent(Register_Activity.this, Login_Activity.class);
+            startActivity(a);
+        }else if(view.getId() == login.getId()) {
+            Toast.makeText(this, "Redirecting to login page", Toast.LENGTH_SHORT).show();
+            a = new Intent(Register_Activity.this, Login_Choice.class);
+            startActivity(a);
+        } else if(view.getId() == create_acc.getId()) {
+            Toast.makeText(this, "Create account button has been pressed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
