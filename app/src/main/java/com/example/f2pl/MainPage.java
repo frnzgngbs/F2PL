5d58de87a727415ec4118a96dd4fb1f9da2fe2ba
Franz Genegobis <genegobis.franz@gmail.com>
2023-05-16 18:10:44 +0800

    MADE SOME ADJUSTMENT SA MPIN ACTIVITY. ALSO ADDED FUNCTIONALITIES SA MGA CARD VIEW. PERO PINAKA IMPORTANTI THAT I MADE FOR THIS COMMIT KAY THE TIMED QUIZ. AS OF NOW SCIENCE CATEGORY PA LANG AKOA NA BUHAT.

--- app/src/main/java/com/example/f2pl/MainPage.java
+++ app/src/main/java/com/example/f2pl/MainPage.java
@@ -1,14 +1,54 @@
 package com.example.f2pl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements View.OnClickListener {
 
+    CardView ctgProfile, ctgExchange, ctgScience, ctgSports, ctgGaming, ctgHistory, ctgProg, ctgMath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        
+        ctgProfile = findViewById(R.id.cardProfile);
+        ctgExchange = findViewById(R.id.cardExchange);
+        ctgScience = findViewById(R.id.cardScience);
+        ctgSports = findViewById(R.id.cardSports);
+        ctgGaming = findViewById(R.id.cardGaming);
+        ctgHistory = findViewById(R.id.cardHistory);
+        ctgProg = findViewById(R.id.cardProg);
+        ctgMath = findViewById(R.id.cardMath);
+        
+        ctgProfile.setOnClickListener(this);
+        ctgExchange.setOnClickListener(this);
+        ctgScience.setOnClickListener(this);
+        ctgSports.setOnClickListener(this);
+        ctgGaming.setOnClickListener(this);
+        ctgHistory.setOnClickListener(this);
+        ctgProg.setOnClickListener(this);
+        ctgMath.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        if (view.getId() == ctgProfile.getId()) Toast.makeText(this, "User Profile", Toast.LENGTH_SHORT).show();
+        else if (view.getId() == ctgExchange.getId()) Toast.makeText(this, "Exchange coins", Toast.LENGTH_SHORT).show();
+        else if (view.getId() == ctgScience.getId()) {
+            i = new Intent(MainPage.this, ScienceForm.class);
+            startActivity(i);
+            Toast.makeText(this, "Science category", Toast.LENGTH_SHORT).show();
+        }
+        else if (view.getId() == ctgSports.getId()) Toast.makeText(this, "Sports category", Toast.LENGTH_SHORT).show();
+        else if(view.getId() == ctgGaming.getId()) Toast.makeText(this, "Gaming category", Toast.LENGTH_SHORT).show();
+        else if (view.getId() == ctgHistory.getId()) Toast.makeText(this, "History category", Toast.LENGTH_SHORT).show();
+        else if (view.getId() == ctgProg.getId()) Toast.makeText(this, "Programming category", Toast.LENGTH_SHORT).show();
+        else if(view.getId() == ctgMath.getId()) Toast.makeText(this, "Mathematics category", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/f2pl/ScienceQuestion.java
+++ app/src/main/java/com/example/f2pl/ScienceQuestion.java
@@ -1,4 +1,43 @@
 package com.example.f2pl;
 
 public class ScienceQuestion {
+
+    public String question[] = {
+            "What is the chemical symbol for the elemenent gold?",
+            "Who developed the theory of relativity?",
+            "What is the largest planet in our solar system?",
+            "What is the process by which plants convert sunlight into energy?",
+            "What is the fundamental of life?",
+            "What is the equation for Einstein's mass-energy equivalence?",
+            "Which scientist discovered the law of gravity?",
+            "What is the process by which a solid changes directly a gas without becoming a liquid first?",
+            "Which planet is known as \"Red Planet\"",
+            "Who is the father of evolution?"
+    };
+
+    public String answer[] = {
+            "Au",
+            "Albert Einstein",
+            "Jupiter",
+            "Photosynthesis",
+            "Cell",
+            "E = mc^2",
+            "Isaac Newton",
+            "Sublimation",
+            "Mars",
+            "Charles Darwin"
+    };
+
+    public String choices[][] = {
+            {"Go", "Ag", "Au", "Go"},
+            {"Isaac Newton", "Albert Einstein", "Marie Curie", "Galileo", "Galilei"},
+            {"Mars", "Venus", "Jupiter", "Saturn"},
+            {"Respiration", "Photosynthesis", "Metabolism", "Digestion"},
+            {"Molecule", "Atom", "Cell", "Gene"},
+            {"E = mc^2", "F = ma", "E = mv^2", "P = mv"},
+            {"Isaac Newton", "Albert Einstein", "Marie Curie", "Nikola Tesla"},
+            {"Melting", "Condensation", "Sublimation", "Evaporation"},
+            {"Mars", "Venus", "Mercury", "Jupitier"},
+            {"Isaac Newton", "Charles Darwin", "Albert Einstein", "Alexander Fleming"}
+    };
 }
